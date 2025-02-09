5fce4396c1c01c0a37a8835857084f8c95f6a3b1
bashar53 <bashar.iiuc77@gmail.com>
2025-02-09 09:26:40 +0600

    Note and User Model added

---
+++ Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes_Management_system.Model;
+
+public class User
+{
+   public string UserId { get; set; } = Guid.NewGuid().ToString();
+   public string Name { get; set; }
+   public string Email { get; set; }
+   public DateTime DateOfBirth { get; set; }
+   public string PasswordHash { get; set; }  // Store hashed password
+   
+}
+
